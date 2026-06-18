81d6793c9da3545434db7655b4325d77c4559a8b
Yosef C Birnbaum <155560788+YosefCh@users.noreply.github.com>
2026-06-17 23:17:35 -0400

    added comment briefly explaining an issue with our original views

--- sql/Combo_Views.sql
+++ sql/Combo_Views.sql
@@ -3,6 +3,8 @@
 BEGIN;
 -- "Combo food" views let you treat multiple ingredients as one logical food.
 -- Name the combo_name in all caps and preface with a dash so it stands out and will be first in the list of foods. 
+---------
+-- Issue with these are that serving sizes are not provided. Must redo with serving sizes as a field.
 CREATE OR REPLACE VIEW YGRT_PB_FRT_NT AS
 select '-YGRT_PB_FRT_NT' AS Combo_Name, name,1 as Combo_id, food_id
 from food where name ilike '%peanut butter%' or name ilike '%greek%' or name ilike '%pecans%' or name ilike '%pear sauce%';
