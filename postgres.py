e01cd9106a8940e933356659a10124c3a9a61f17
Yosef C Birnbaum <155560788+YosefCh@users.noreply.github.com>
2026-09-07 16:22:10 -0400

    Constants for LIVE and DEV db names and added a cell atop all new notebook files to show the environment.

--- postgres.py
+++ postgres.py
@@ -7,7 +7,11 @@ import os
 import json
 
 ADMIN_DB_NAME = "postgres"
-DB_NAME = "DEV_wellness_tracker"
+
+LIVE_DB_NAME = "LIVE_wellness_tracker"
+DEV_DB_NAME = "DEV_wellness_tracker"
+
+DB_NAME = DEV_DB_NAME  # default database name for local development
 CREATE_TABLES_FILE = r"C:\Users\Rebecca\OneDrive\Documents\Python AI\Wellness Tracker\sql\Create_tables.sql"
 POPULATE_DIM_TABLES_FILE = r"C:\Users\Rebecca\OneDrive\Documents\Python AI\Wellness Tracker\sql\Populate_dim_tables.sql"
 # currently not using this approach, (using a dropdown in the GUI instead), but keeping it here for now in case I want to use it later.
